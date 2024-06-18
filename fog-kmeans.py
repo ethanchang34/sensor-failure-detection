8743eafefa5bbcf179530acc32760ffeb40249a8
Ethan <ethanchang34@yahoo.com>
2024-06-18 02:24:08 -0400

    kmeans for n=3 only

--- fog-kmeans.py
+++ fog-kmeans.py
@@ -1,16 +1,26 @@
 import numpy as np
+import torch
 from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import pickle
 
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
+
+# print(f"Using device: {device}")
+
 with open('Flagged Data/flagged_data.pkl', 'rb') as file:
     flagged_data = pickle.load(file)
 
 num_sensors = 7 # REPLACE with dynamic code
 
-sensor_ids = flagged_data[next(iter(flagged_data))].keys()
+sensor_ids = list(flagged_data[next(iter(flagged_data))].keys())
 
 # Initialize an empty list to store flattened data
 flattened_data = {sensor_id: [] for sensor_id in sensor_ids}
@@ -39,7 +49,6 @@ def dtw_correlation(data):
 dtw_dists = dtw_correlation(sensor_data)
 print(dtw_dists)
 
-'''
 # Number of clusters
 num_clusters = 3
 kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(dtw_dists)
@@ -54,11 +63,14 @@ print("Cluster Centers:\n", kmeans.cluster_centers_)
 
 # Visualize the clustering result
 for i in range(num_clusters):
-    plt.plot(kmeans.cluster_centers_[i], label=f'Cluster {i}')
+    plt.scatter(sensor_ids, kmeans.cluster_centers_[i], label=f'Cluster {i}')
+
 plt.legend()
+plt.xlabel('Sensor IDs')
+plt.ylabel('Cluster Center Value')
+plt.title('Cluster Centers Visualization')
 plt.show()
 
 # Print the cluster labels for each sensor
 for sensor_id, label in zip(sensor_ids, labels):
     print(f'Sensor {sensor_id} is in Cluster {label}')
-    '''
